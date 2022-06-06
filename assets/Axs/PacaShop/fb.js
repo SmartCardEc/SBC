f36f496ae4d2a10763ec15064c9a8d1d84578477
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-06 10:57:47 -0500

    Update

--- assets/Axs/PacaShop/fb.js
+++ assets/Axs/PacaShop/fb.js
@@ -1,16 +1,17 @@
 const ua = detect.parse(navigator.userAgent)
-
+const buttonIg = document.querySelector('.Instagram')
 const buttonFb = document.querySelector('.Facebook')
 
 
 if(ua.device!==null){
 
     if(ua.device.family==='Android'){
+    buttonFb.href = 'instagram://user?username=paca___shop'    
     buttonFb.href = 'fb://page/106213221522147'
     }
 
     if (ua.device.family === 'iPhone' || ua.device.family === 'iPad') {
+    buttonFb.href = 'instagram://user?username=paca___shop'
     buttonFb.href = 'fb://page/?id=106213221522147'
-
 }
 }
