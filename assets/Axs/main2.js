f35df94a3829020c3dd44bcd5e4010212b738bac
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-06 09:48:09 -0500

    Final

--- assets/Axs/main2.js
+++ assets/Axs/main2.js
@@ -1,15 +1,18 @@
 const ua = detect.parse(navigator.userAgent)
-const button = document.querySelector('.Facebook')
 
+const buttonFb = document.querySelector('.Facebook')
+const buttonIg = document.querySelector('.Facebook')
 
 
 if(ua.device!==null){
 
     if(ua.device.family==='Android'){
-    button.href = 'fb://page/106213221522147'
+    buttonFb.href = 'fb://page/106213221522147'
+    buttonIg.href = 'instagram://user?username=paca___shop'
     }
 
     if (ua.device.family === 'iPhone' || ua.device.family === 'iPad') {
-    button.href = 'fb://page/?id=106213221522147'
+    buttonFb.href = 'fb://page/?id=106213221522147'
+    buttonIg.href = 'instagram://user?username=paca___shop'
 }
 }
