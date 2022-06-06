6b2e8e692fdb632176eaedc3d90e84f88b6eb26f
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-06 11:17:49 -0500

    uodate

--- assets/Axs/PacaShop/fb.js
+++ assets/Axs/PacaShop/fb.js
@@ -6,12 +6,12 @@ const buttonFb = document.querySelector('.Facebook')
 if(ua.device!==null){
 
     if(ua.device.family==='Android'){
-    buttonFb.href = 'instagram://user?username=paca___shop'    
+    buttonIg.href = 'instagram://user?username=paca___shop'    
     buttonFb.href = 'fb://page/106213221522147'
     }
 
     if (ua.device.family === 'iPhone' || ua.device.family === 'iPad') {
-    buttonFb.href = 'instagram://user?username=paca___shop'
+    buttonIg.href = 'instagram://user?username=paca___shop'
     buttonFb.href = 'fb://page/?id=106213221522147'
 }
 }
