f57cebc8b3ad7c346e9cedb52882015fbe4a7779
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-06 09:42:10 -0500

    Final Paca

--- assets/Axs/main2.js
+++ assets/Axs/main2.js
@@ -1,6 +1,15 @@
 const ua = detect.parse(navigator.userAgent)
-const button = document.querySelector('.btn')
+const button = document.querySelector('.Facebook')
 
-if (ua.device.family === 'iPhone' || ua.device.family === 'iPad') {
-  button.textContent = 'Facebook iOS'
+
+
+if(ua.device!==null){
+
+    if(ua.device.family==='Android'){
+    button.href = 'fb://page/106213221522147'
+    }
+
+    if (ua.device.family === 'iPhone' || ua.device.family === 'iPad') {
+    button.href = 'fb://page/?id=106213221522147'
+}
 }
