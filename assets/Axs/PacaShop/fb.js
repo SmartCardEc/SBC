7712912bc82b4e7c6e41953dfa41790b726d6f12
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-08 22:26:07 -0500

    Update

--- assets/Axs/PacaShop/fb.js
+++ assets/Axs/PacaShop/fb.js
@@ -7,12 +7,12 @@ const buttonIg = document.querySelector('.Instagram')
 if(ua.device!==null){
 
     if(ua.device.family==='Android'){   
-    buttonIg.href = 'instagram://user?username=smartcard.ec'
-    buttonFb.href = 'fb://page/109165095124709'
+    buttonIg.href = 'instagram://user?username=paca___shop'
+    buttonFb.href = 'fb://page/106213221522147'
     }
 
     if (ua.device.family === 'iPhone' || ua.device.family === 'iPad' || ua.device.family === 'Nexus 5') {
-    buttonIg.href = 'instagram://user?username=smartcard.ec'
-    buttonFb.href = 'fb://page/?id=109165095124709'
+    buttonIg.href = 'instagram://user?username=paca___shop'
+    buttonFb.href = 'fb://page/?id=106213221522147'
 }
 }
