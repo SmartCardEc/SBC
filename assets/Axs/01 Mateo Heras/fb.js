5ecfe67d3e3e281620d898257476074ac42f5d49
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-08 21:29:55 -0500

    Update

--- assets/Axs/01 Mateo Heras/fb.js
+++ assets/Axs/01 Mateo Heras/fb.js
@@ -6,19 +6,12 @@ if(ua.device!==null){
 
     if(ua.device.family==='Android'){   
     buttonFb.href = 'fb://page/109165095124709'
+    buttonIg.href = 'instagram://user?username=smartcard.ec'
     }
+
 
     if (ua.device.family === 'iPhone' || ua.device.family === 'iPad') {
     buttonFb.href = 'fb://page/?id=109165095124709'
-}
-}
-
-const ua2 = detect.parse(navigator.userAgent)
-
-
-if(ua2.device!==null){
-
-    if (ua.device.family !== 'iPhone' || ua.device.family !== 'iPad' || ua.device.family == 'Android' ) {
-    buttonIg.href = 'https://www.instagram.com/smartcard.ec/'
-}
+    buttonIg.href = 'instagram://user?username=smartcard.ec'    
+    }
 }
