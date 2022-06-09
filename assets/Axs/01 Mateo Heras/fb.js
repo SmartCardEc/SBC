48fb511a39865761d2232be9b875bee903305d23
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-08 22:15:30 -0500

    Update

--- assets/Axs/01 Mateo Heras/fb.js
+++ assets/Axs/01 Mateo Heras/fb.js
@@ -17,4 +17,3 @@ if(ua.device!==null){
 }
 }
 
-console.log(ua)
--- assets/Axs/PacaShop/fb.js
+++ assets/Axs/PacaShop/fb.js
@@ -1,14 +1,18 @@
 const ua = detect.parse(navigator.userAgent)
 const buttonFb = document.querySelector('.Facebook')
+const buttonIg = document.querySelector('.Instagram')
+
 
 
 if(ua.device!==null){
 
     if(ua.device.family==='Android'){   
-    buttonFb.href = 'fb://page/106213221522147'
+    buttonIg.href = 'instagram://user?username=smartcard.ec'
+    buttonFb.href = 'fb://page/109165095124709'
     }
 
-    if (ua.device.family === 'iPhone' || ua.device.family === 'iPad') {
-    buttonFb.href = 'fb://page/?id=106213221522147'
+    if (ua.device.family === 'iPhone' || ua.device.family === 'iPad' || ua.device.family === 'Nexus 5') {
+    buttonIg.href = 'instagram://user?username=smartcard.ec'
+    buttonFb.href = 'fb://page/?id=109165095124709'
 }
 }
