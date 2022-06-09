fb7da2bd23f706add9904878d9e88ed68b707084
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-08 21:10:14 -0500

    Update

--- assets/Axs/01 Mateo Heras/fb.js
+++ assets/Axs/01 Mateo Heras/fb.js
@@ -1,7 +1,6 @@
 const ua = detect.parse(navigator.userAgent)
 const buttonFb = document.querySelector('.Facebook')
 
-
 if(ua.device!==null){
 
     if(ua.device.family==='Android'){   
@@ -16,10 +15,9 @@ if(ua.device!==null){
 const ua2 = detect.parse(navigator.userAgent)
 const buttonIg = document.querySelector('.Instagram')
 
+if(ua2.device!==null){
 
-if(ua.device!==null){
-
-    if (ua2.device.family === 'iPhone' || ua2.device.family === 'iPad'|| ua2.device.family === 'Android') {
-    buttonIg.href = 'instagram://user?username=smartcard.ec'
+    if (ua2.device.family === 'chrome' || ua2.device.family === 'firefox' || ua2.device.family === 'opera' || ua2.device.family === 'safari') {
+    buttonIg.href = 'https://www.instagram.com/smartcard.ec/'
 }
 }
