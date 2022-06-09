38df76b0d92c31a007b1d8691811f8d70fc16d3f
MateoHeras77 <smartcard.ec@gmail.com>
2022-06-08 20:30:40 -0500

    Mateo Heras

---
+++ assets/Axs/01 Mateo Heras/fb.js
@@ -0,0 +1,14 @@
+const ua = detect.parse(navigator.userAgent)
+const buttonFb = document.querySelector('.Facebook')
+
+
+if(ua.device!==null){
+
+    if(ua.device.family==='Android'){   
+    buttonFb.href = 'fb://page/109165095124709'
+    }
+
+    if (ua.device.family === 'iPhone' || ua.device.family === 'iPad') {
+    buttonFb.href = 'fb://page/?id=109165095124709'
+}
+}
