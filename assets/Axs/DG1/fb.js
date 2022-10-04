015545bc5ff3b6ca6b5b9cfdb09a7a299939ef64
MateoHeras77 <66641868+MateoHeras77@users.noreply.github.com>
2022-10-04 18:05:23 -0500

    Update

--- assets/Axs/DG1/fb.js
+++ assets/Axs/DG1/fb.js
@@ -7,12 +7,12 @@ const buttonIg = document.querySelector('.Instagram')
 if(ua.device!==null){
 
     if(ua.device.family==='Android'){   
-    buttonIg.href = 'instagram://user?username=smartcard.ec'
-    buttonFb.href = 'fb://page/109165095124709'
+    buttonIg.href = 'instagram://user?username=duragasec'
+    buttonFb.href = 'fb://page/693235311042557'
     }
 
     if (ua.device.family === 'iPhone' || ua.device.family === 'iPad' || ua.device.family === 'Nexus 5') {
-    buttonIg.href = 'instagram://user?username=smartcard.ec'
-    buttonFb.href = 'fb://page/?id=109165095124709'
+    buttonIg.href = 'instagram://user?username=duragasec'
+    buttonFb.href = 'fb://page/?id=693235311042557'
 }
 }
